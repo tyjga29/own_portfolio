b766d9770e2d78badc82343e0c688d9681e911c8
tyjga29 <101278206+tyjga29@users.noreply.github.com>
2024-03-20 15:06:54 +0100

    Update cvData.js

--- tobi-portfolio/src/app/cv/cvData.js
+++ tobi-portfolio/src/app/cv/cvData.js
@@ -1,33 +1,64 @@
 export const data = [
     {
         title: '- July 2017',
-        content: 'Highschool Albert Schweitzer Gymnasium in Leonberg',
+        content: (
+          <>
+            Highschool Albert Schweitzer Gymnasium<br />
+            <i>Leonberg</i>
+          </>
+        ),
       },
       {
         title: 'August 2017',
-        content: 'European Bartender School in Barcelona',
+        content: (
+          <>
+            European Bartender School<br />
+            <i>Barcelon</i>
+          </>
+        ),
         image: '/pictures/cv/ebs.png',
         imageWidth: 130,
         imageHeight: 130,
       },
       {
         title: 'September 2017 - July 2021',
-        content: 'Studies of Sports and Culture Management at Hochschule Heilbronn',
+        content: (
+          <>
+            Studies of Sports and Culture Management at Hochschule Heilbronn<br />
+            <i>Künzelsau</i>
+          </>
+        ),
         image: '/pictures/cv/kiau.webp',
         imageWidth: 400,
         imageHeight: 130,
       },
       {
         title: 'February - July 2019',
-        content: 'First board of student club, faculty council, and academic commission at Hochschule Heilbronn',
+        content: (
+          <>
+            First board of student club, faculty council, and academic commission  at Hochschule Heilbronn<br />
+            <i>Künzelsau</i>
+          </>
+        ),
       },
       {
         title: 'September 2018 - July 2019',
-        content: 'Voluntary tutoring for immigrants',
+          
+        content: (
+          <>
+            Voluntary tutoring for immigrants<br />
+            <i>Künzelsau</i>
+          </>
+        ),
       },
       {
         title: 'May 2019',
-        content: 'Investigation of the Porosea triathlon in Poros, Greece, with subsequent lecture in front of the city',
+        content: (
+          <>
+            Investigation of the Porosea triathlon with subsequent lecture in front of the city<br />
+            <i>Poros, Greece</i>
+          </>
+        ),
         image: '/pictures/cv/poros.jpg',
         imageWidth: 450,
         imageHeight: 150,
@@ -36,43 +67,69 @@ export const data = [
         title: 'February - May 2020',
         content: (
           <>
-            Internship at Elephant Gin in Berlin as Event and Product Manager<br />
+            Internship at Elephant Gin as Event and Product Manager<br />
             Bar Convent Berlin 2020<br />
             Grüne Woche 2020<br />
+            <i>Berlin</i>
           </>
         ),
       },
       {
         title: 'February - May 2020',
-        content: 'Semester abroad at the Universidade Federal de Pernambuco in Recife, Brazil',
+        content: (
+          <>
+            Semester abroad at the Universidade Federal de Pernambuco<br />
+            <i>Recife, Brazil</i>
+          </>
+        ),
         image: '/pictures/cv/uPernambuco.jpg',
         imageWidth: 400,
         imageHeight: 150,
       },
       {
         title: 'May 2020',
-        content: 'Organization of the streaming concert "Klassik unter Strom" in Berlin',
+        content: (
+          <>
+            Organization of the streaming concert "Klassik unter Stron"<br />
+            <i>Berlin</i>
+          </>
+        ),
         image: '/pictures/cv/unterStrom.jpg',
         imageWidth: 400,
         imageHeight: 130,
       },
       {
         title: 'September 2021 - August 2024',
-        content: 'Dual Studies of Computer Science at Duale Hochschule Baden-Württemberg in Stuttgart at diconium digital solutions',
+        content: (
+          <>
+            Dual Studies of Computer Science at Duale Hochschule Baden-Württemberg at diconium digital solutions<br />
+            <i>Stuttgart</i>
+          </>
+        ),
         image: '/pictures/cv/dhbw.jpg',
         imageWidth: 400,
         imageHeight: 150,
       },
       {
         title: 'September 2022 - August 2023',
-        content: 'Gin expert at Botanical Affairs in Stuttgart',
+        content: (
+          <>
+            Gin expert at Botanical Affairsbr />
+            <i>Stuttgart</i>
+          </>
+        ),
         image: '/pictures/cv/bot.jpg',
         imageWidth: 250,
         imageHeight: 150,
       },
       {
         title: 'September 2023 - September 2024',
-        content: 'Bartender at Le Petit Coq in Stuttgart',
+        content: (
+          <>
+            Bartender at Le Petit Coq<br />
+            <i>Stuttgart</i>
+          </>
+        ),
         image: '/pictures/cv/petit.jpg',
         imageWidth: 250,
         imageHeight: 150,
